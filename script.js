96f9ad412a171e6e8303c73ba04ae28ec74fc906
aadhithbala <aadhi66613@gmail.com>
2023-07-14 19:43:26 +0530

    Create a hover function & Declare grids nodelist in global scope
     Hover function has been created as a separate function will be called
     from drawGrids function. Declared the grids nodelist in global scope
     for easier access by both eraseGrids and Hover functions

--- script.js
+++ script.js
@@ -6,19 +6,36 @@ const resetButton = document.querySelector('.reset-grids');
 
 let dimension = 16;
 
-let gridCount = dimension ** 2;
+let grids;
+
+let totalGrids = dimension ** 2;
 
 let gridSize = containerHeight / dimension;
 
+//Hover Effect Function
+const leaveTrial = function () {
+  for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener('mouseover', function () {
+      grids[i].classList.add('hover-effect');
+    });
+  }
+};
+
 const drawGrids = function (dimension) {
-  for (let i = 0; i < gridCount; i++) {
+  for (let i = 0; i < totalGrids; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grids');
     grid.setAttribute('style', `width:${gridSize}px;height:${gridSize}px`);
     container.appendChild(grid);
   }
+
+  grids = document.querySelectorAll('.grids');
+  leaveTrial();
+
+  console.log(`TotalGrids: ${totalGrids}, GridSize: ${gridSize}px`);
 };
 
+//Erases grids by iterating over the nodelist
 const eraseGrids = function () {
   grids = document.querySelectorAll('.grids');
   for (let i = 0; i < grids.length; i++) {
@@ -26,29 +43,16 @@ const eraseGrids = function () {
   }
 };
 
-drawGrids(dimension);
-
-console.log(`Total Squares: ${gridCount}, GridSize: ${gridSize}px`);
-
-let grids = document.querySelectorAll('.grids');
-
-//Change bg-color while mouse enters
-for (let i = 0; i < grids.length; i++) {
-  grids[i].addEventListener('mouseover', function () {
-    grids[i].classList.add('hover-effect');
-  });
-}
+drawGrids(dimension); //Invoking function to create default 16x16 grid at the start
 
 resetButton.addEventListener('click', function () {
   dimension = Number(prompt('Enter the dimension of the grid'));
-  gridCount = dimension ** 2;
-  gridSize = containerHeight / dimension;
-
-  console.log(`Total Squares: ${gridCount}, GridSize: ${gridSize}px`);
-
   while (dimension > 50 || dimension === 0) {
     dimension = Number(prompt('The dimension should be between 1 to 50'));
   }
+
+  totalGrids = dimension ** 2;
+  gridSize = containerHeight / dimension;
   eraseGrids();
   drawGrids(dimension);
 });
